bcf40283d12c9c1f612cc4226173bcba6955b252
TextError <savinraul1983@yahoo.com>
2019-09-02 17:48:53 +0100

    Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react";
 
-export default () => <div>Hello world!</div>
+import Layout from '../layout/Layout';
+
+export default () => (
+  <>
+    <Layout>
+      hello world
+    </Layout>
+  </>
+)

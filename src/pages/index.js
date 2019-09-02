9721d28f0a04b03002fd3723f3db00c6b6f32985
TextError <savinraul1983@yahoo.com>
2019-09-02 18:26:28 +0100

    gatsby sass and boostrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,10 @@ import React from "react";
 import Layout from '../layout/Layout';
 
 export default () => (
-  <>
-    <Layout>
-      hello world
-    </Layout>
-  </>
+  <Layout>
+    <div className='row no-gutters'>
+      <div className='col-6'>test</div>
+      <div className='col-6'>test</div>
+    </div>
+  </Layout>
 )

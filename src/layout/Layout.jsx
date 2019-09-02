f0c3e7f969b98bfb417d75e4116cc74f5f305968
TextError <savinraul1983@yahoo.com>
2019-09-02 18:35:57 +0100

    setup

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Navbar from './navbar/Navbar';
 import Footer from './footer/Footer';
 
+import './layout.css'
+
 const Layout = ({ children }) => {
   return (
     <>

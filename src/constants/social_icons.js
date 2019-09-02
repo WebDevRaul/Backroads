000fbff353fa2463b715fd71ba664ed2b8e131ec
TextError <savinraul1983@yahoo.com>
2019-09-02 18:55:17 +0100

    react-icons

---
+++ src/constants/social_icons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {FaFacebook, FaTwitterSquare, FaSuarespace} from 'react-icons';
+
+export default [
+  {
+    icon: <FaFacebook />,
+    url: 'https://google.com'
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: 'https://google.com'
+  },
+  {
+    icon: <FaSuarespace />,
+    url: 'https://google.com'
+  }
+]

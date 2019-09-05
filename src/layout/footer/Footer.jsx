4a80741025fdd5283a2c226e4dafc059437a2fde
TextError <savinraul1983@yahoo.com>
2019-09-05 10:19:35 +0100

    footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+
+import links from '../../constants/links';
+import icons from '../../constants/social_icons';
+import { Link } from 'gatsby';
+
+import './footer.scss';
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
+    <footer className='footer'>
+      <div className='links'> 
+        <ul>
+          {links.map((item, index) => {
+            const { path, text } = item;
+            return <li key={index}><Link to={path} />{text}</li>
+          })}
+        </ul>
+      </div>
+      <div className='icons'>
+        {icons.map((item, index) => {
+            const {icon, url} = item;
+            return <a key={index} href={url} target='_blank' rel='noopener noreferrer'>{icon}</a>
+        })}
+      </div>
+      <div className='copy-right'>
+        copyright &copy; backroads travel company {new Date().getFullYear()} all rights reserved
+      </div>
+    </footer>
   )
 }
 

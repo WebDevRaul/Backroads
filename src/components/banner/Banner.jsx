c455f8667dfc7aa4c5efeb7ffd7a02aa61eb3f5c
TextError <savinraul1983@yahoo.com>
2019-09-05 14:25:18 +0100

    finish later

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -7,15 +7,17 @@ const Banner = ({ title, info, children }) => {
   return (
     <div className='banner'>
       <h1>{title}</h1>
-      <p>{info}</p>
       {children}
     </div>
   )
 };
 
+Banner.defaultProps = {
+  info: PropTypes.string
+}
+
 Banner.propTypes = {
   title: PropTypes.string.isRequired,
-  info: PropTypes.string.isRequired,
   children: PropTypes.object.isRequired
 };
 
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../layout/Layout';
+import Banner from '../components/banner/Banner';
+import { primaryColor } from '../layout/variables.scss';
+
+const errorStyle = {
+  backgroundColor: primaryColor,
+  minHeight: 'calc(100vh - 62px)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
 
 const error = () => {
   return (
-    <div>
-      error
-    </div>
+    <Layout>
+      {console.log(primaryColor)}
+      <header style={errorStyle}>
+        <Banner title='oops it`s a dead end'>
+          <Link to='/' className='btn-white'>back to home page</Link>
+        </Banner>
+      </header>
+    </Layout>
   )
 }
 

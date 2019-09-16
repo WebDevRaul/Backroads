bca25934a361070e0f7d3fd14a772544f523536b
TextError <savinraul1983@yahoo.com>
2019-09-16 16:12:12 +0100

    styledHero

---
+++ src/components/common/styledHero/Styled_Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import BackgroundImage from 'gatsby-background-image';
+
+import './styledHero.scss';
+
+const StyledHero = ({ img, className, children, home }) => {
+  return (
+    <div className={classnames('styled-hero', {'bg-linear': home})}>
+      <BackgroundImage className={className} fluid={img} home={home}>
+        {children}
+      </BackgroundImage>
+    </div>
+  )
+};
+
+StyledHero.propTypes = {
+  img: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  children: PropTypes.object.isRequired,
+  home: PropTypes.bool.isRequired
+}
+
+export default StyledHero;

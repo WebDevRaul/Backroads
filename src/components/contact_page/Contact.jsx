2775d227ec9950a941f208ec30af2bfad52b38e6
TextError <savinraul1983@yahoo.com>
2019-09-19 12:07:20 +0100

    formspree

--- src/components/contact_page/Contact.jsx
+++ src/components/contact_page/Contact.jsx
@@ -7,14 +7,17 @@ const Contact = () => (
   <section className='contact'>
     <Title title='contact' subtitle='us' />
     <div className='center'>
-      <form noValidate className='form'>
+      <form noValidate action='https://formspree.io/test@test.com' method='POST' className='form'>
         <div>
+          <label htmlFor='name'>name</label>
           <input type='text' name='name' value='' id='name' className='form-control' placeholder='Jhon smith' />
         </div>
         <div>
+          <label htmlFor='email'>email</label>
           <input type='email' name='email' value='' id='email' className='form-control' placeholder='Jhon smith@email.com' />
         </div>
         <div>
+          <label htmlFor='message'>message</label>
           <textarea name='message' id='message' rows='10' className='form-control' placeholder='hello there'/>
         </div>
         <div>

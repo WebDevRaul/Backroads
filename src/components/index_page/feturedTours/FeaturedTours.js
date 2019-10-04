4a5c557c683c3c13639f170c23050bc5f5b7f5ac
TextError <savinraul1983@yahoo.com>
2019-10-04 10:21:33 +0100

    styled featured css

--- src/components/index_page/feturedTours/FeaturedTours.js
+++ src/components/index_page/feturedTours/FeaturedTours.js
@@ -4,19 +4,24 @@ import AniLink from 'gatsby-plugin-transition-link/AniLink';
 
 import Tour from '../../common/tour/Tour';
 import Title, { } from '../../common/title/Title';
+import StyledFeatured from './StyledFeatured';
 
 const FeaturedTours = () => {
   const response = useStaticQuery(getTours);
   const { edges } = response.featuredTours;
 
   return (
-    <section className='tours'>
-      <Title title='featured' subtitle='tours' />
-      {
-        edges.map(({ node }), index => <Tour key={index} tour={node} />)
-      }
-      <AniLink fade to='/tours' className='btn btn-primary'>all tours</AniLink>
-    </section>
+    <StyledFeatured>
+      <section className='tours'>
+        <Title title='featured' subtitle='tours' />
+        <div className='center'>
+          {
+            edges.map(({ node }), index => <Tour key={index} tour={node} />)
+          }
+        </div>
+        <AniLink fade to='/tours' className='btn btn-primary'>all tours</AniLink>
+      </section>
+    </StyledFeatured>
   )
 }
 

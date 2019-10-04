ecf5bb68cd6f5ef93598037e40fdad22eb12a8cd
TextError <savinraul1983@yahoo.com>
2019-10-04 10:29:48 +0100

    finish later

--- src/components/common/tour/Tour.js
+++ src/components/common/tour/Tour.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Image from 'gatsby-image';
+import AniLink from 'gatsby-plugin-transition-link';
 
-const Tour = () => {
+import { FaMap } from 'react-icons/fa';
+import StyledTour from './StyledTour';
+
+const Tour = ({ tour }) => {
+  const { name, price, country, days, slug, images } = tour;
   return (
-    <div>
-      
-    </div>
+    <StyledTour>
+
+    </StyledTour>
   )
 }
 
